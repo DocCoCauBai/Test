d47b4301a8427cbd68a7007466d0c7e963e05ef9
Bảo Bảo <>
2021-08-27 00:57:20 +0700

    - Fix conflict

--- DemoNewIOS/DemoNewIOS/ContentView.swift
+++ DemoNewIOS/DemoNewIOS/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello SwiftUI, world!")
-        .padding().foregroundColor(.yellow)
-
+        .padding().foregroundColor(.orange)
     }
 }
 
