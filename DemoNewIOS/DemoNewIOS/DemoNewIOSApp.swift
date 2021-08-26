b6b9df22741520150737bcd21a38ec29b806cf71
Bảo Bảo <>
2021-08-26 23:57:30 +0700

    + Init project

---
+++ DemoNewIOS/DemoNewIOS/DemoNewIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNewIOSApp.swift
+//  DemoNewIOS
+//
+//  Created by Bảo Bảo on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoNewIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
