a7cc0b2a7861da968b135f5f3b60702f7c0521c8
Bảo Bảo <>
2021-08-27 01:22:13 +0700

    + Add button

--- DemoNewIOS/DemoNewIOS/ContentView.swift
+++ DemoNewIOS/DemoNewIOS/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     var body: some View {
         Text("Hello SwiftUI, world!")
         .padding().foregroundColor(.orange)
+        Button("Button") {
+            print("tap button")
+        }
     }
 }
 
