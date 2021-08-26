ebe5652189ba8ca61b0310a7994fa990cf323d50
Bảo Bảo <>
2021-08-27 01:55:00 +0700

    - Fix conflict

--- DemoNewIOS/DemoNewIOS/ContentView.swift
+++ DemoNewIOS/DemoNewIOS/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello SwiftUI")
         .padding().foregroundColor(.green)
+        Label("Label", image: "").foregroundColor(.blue)
         Button("Button") {
             print("tap button")
         }
