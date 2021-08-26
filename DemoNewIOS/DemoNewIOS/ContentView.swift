7e290e425d78fd7cd1482905767f073154ac2f30
Bảo Bảo <>
2021-08-27 01:34:07 +0700

    - Change text SwiftUI

--- DemoNewIOS/DemoNewIOS/ContentView.swift
+++ DemoNewIOS/DemoNewIOS/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("BaoPT")
+        Text("Hello SwiftUI")
         .padding().foregroundColor(.orange)
         Button("Button") {
             print("tap button")
