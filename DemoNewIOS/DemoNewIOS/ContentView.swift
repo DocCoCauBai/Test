422261bf4437394f12e2f0e3acea8a2b3bf9f9d0
Bảo Bảo <>
2021-08-27 00:48:22 +0700

    - Change text SwiftUI

--- DemoNewIOS/DemoNewIOS/ContentView.swift
+++ DemoNewIOS/DemoNewIOS/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello SwiftUI, world!")
         .padding().foregroundColor(.yellow)
 
     }
